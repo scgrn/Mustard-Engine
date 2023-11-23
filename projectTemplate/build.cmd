bd448040b590d4d25a9aef08b0df17e6253ca337
Andrew Krause <ajkrause@gmail.com>
2023-11-23 10:27:07 -0500

    Update project template build scripts

--- projectTemplate/build.cmd
+++ projectTemplate/build.cmd
@@ -10,6 +10,10 @@ if "%config%" == "debug" (
     call:buildDebug
 ) else if "%config%" == "release" (
     call:buildRelease
+) else if "%config%" == "web-debug" (
+    call:buildWebDebug
+) else if "%config%" == "web-release" (
+    call:buildWebRelease
 ) else if "%config%" == "assets" (
     call:buildAssets
 ) else if "%config%" == "dist" (
@@ -17,6 +21,8 @@ if "%config%" == "debug" (
 ) else if "%config%" == "all" (
     call:buildDebug
     call:buildRelease
+    call:buildWebDebug
+    call:buildWebRelease
     call:buildAssets
 ) else (
     echo usage: build config
@@ -25,6 +31,8 @@ if "%config%" == "debug" (
     echo.
     echo debug
     echo release
+    echo web-debug
+    echo web-release
     echo assets
     echo dist
     echo all
@@ -53,12 +61,32 @@ exit /b 0
     cmake --build build\release
 exit /b 0
 
+:buildWebDebug
+    echo Building %PROJECT_NAME% for web in debug mode...
+    
+    md build\web-debug 2> nul
+    cd build\web-debug
+    call emcmake cmake ..\.. -DCMAKE_BUILD_TYPE=Debug -DPROJECT_NAME=%PROJECT_NAME% -DMUSTARD_DIR=%MUSTARD_PATH% -DSDL2_DIR=%SDL2_PATH%
+    cd ..\..
+    cmake --build build\web-debug
+exit /b 0
+
+:buildWebRelease
+    echo Building %PROJECT_NAME% for web in release mode...
+
+    md build\web-release 2> nul
+    cd build\web-release
+    call emcmake cmake ..\.. -DCMAKE_BUILD_TYPE=Release -DPROJECT_NAME=%PROJECT_NAME% -DMUSTARD_DIR=%MUSTARD_PATH% -DSDL2_DIR=%SDL2_PATH%
+    cd ..\..
+    cmake --build build\web-release
+exit /b 0
+
 :buildAssets
     echo Building asset archive...
 
     set MUSTARD_PATH=%MUSTARD_PATH:/=\%
     cd assets
-    ..\%MUSTARD_PATH%\bin\Mustard-AssetCompiler ..\%PROJECT_NAME%.dat %ENCYPTION_KEY%
+    ..\%MUSTARD_PATH%\bin\Mustard-AssetCompiler ..\bin\%PROJECT_NAME%.dat %ENCYPTION_KEY%
     cd ..
 exit /b 0
 
