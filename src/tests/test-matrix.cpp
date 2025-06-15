be53e7d7a9f6781cea74e0ba2600d7c816278dfc
Andrew Krause <ajkrause@gmail.com>
2025-06-14 23:55:59 -0400

    Fix multiplication test for column-major

--- src/tests/test-matrix.cpp
+++ src/tests/test-matrix.cpp
@@ -23,14 +23,14 @@ static void testMultiplication() {
 
     suite.assert(a * b == c, "Identity multiplication");
 
-    float data[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    float data[] = {1, 4, 7, 2, 5, 8, 3, 6, 9};
     a = AB::Mat3(data);
     suite.assert(a * b == a, "Arbitrary x identity multiplication");
     suite.assert(b * a == a, "Arbitrary x identity multiplication");
 
-    float data2[] = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+    float data2[] = {9, 6, 3, 8, 5, 2, 7, 4, 1};
     b = AB::Mat3(data2);
-    float data3[] = {30, 24, 18, 84, 69, 54, 138, 114, 90};
+    float data3[] = {30, 84, 138, 24, 69, 114, 18, 54, 90};
     c = AB::Mat3(data3);
     suite.assert(a * b == c, "Known values multiplication");
 }
