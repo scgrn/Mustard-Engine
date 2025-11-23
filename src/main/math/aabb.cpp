7f2489eb5d27ba8f95a8fb1c22c1598ff3dd1097
Andrew Krause <ajkrause@gmail.com>
2025-11-23 17:18:21 -0500

    Fix AABB collision test

--- src/main/math/aabb.cpp
+++ src/main/math/aabb.cpp
@@ -54,12 +54,12 @@ void AABB::calculateCenter() {
 
 b8 AABB::collides(const AABB &other) {
     return(
-        max.x > other.min.x &&
-        min.x < other.max.x &&
-        max.y > other.min.y &&
-        min.y < other.max.y &&
-        max.z > other.min.z &&
-        min.z < other.max.z);
+        max.x >= other.min.x &&
+        min.x <= other.max.x &&
+        max.y >= other.min.y &&
+        min.y <= other.max.y &&
+        max.z >= other.min.z &&
+        min.z <= other.max.z);
 }
 
 std::array<AABB, 8> AABB::subdivide() {
